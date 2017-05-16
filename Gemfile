cf1c486d5d86b73e21c391d86ce296d45bdd2681
Vincent Chan <vincent.chan@biola.edu>
2017-05-16 10:00:43 -0700

    webpack and bundle setup

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ group :development, :test do
 end
 
 group :development do
+  gem 'pry-rails'
+  gem 'binding_of_caller'
+  gem 'better_errors'
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '~> 3.0.5'
---
+++ frontend/entry.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Root from './components/root';
+// import configureStore from './store/store';
+//
+// import * as actions from './actions/post_actions';
+// import { selectAllPosts } from './reducers/selectors';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  // const store = configureStore();
+
+  // window.store = store;
+
+  ReactDOM.render(<h1>hi</h1>, root);
+});
