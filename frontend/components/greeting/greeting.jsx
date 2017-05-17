e1dfe3316919546562eba6a884f1b0c352252a2d
Vincent Chan <vincent.chan@biola.edu>
2017-05-17 06:59:32 -0700

    signout button functional

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -19,4 +19,4 @@ const Greeting = ({ currentUser, logout }) => (
   currentUser ? personalGreeting(currentUser, logout) : sessionLinks()
 );
 
-export default Greetings;
+export default Greeting;
--- frontend/components/greeting/greeting_container.js
+++ frontend/components/greeting/greeting_container.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import { logout } from '../../actions/session_actions';
-import Greeting from '.greeting';
+import Greeting from './greeting';
 
 const mapStateToProps = ( {session} ) => ({
   currentUser: session.currentUser
