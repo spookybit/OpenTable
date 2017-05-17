af02b0923579dbe0f81093232eab183ed776b645
Vincent Chan <vincent.chan@biola.edu>
2017-05-16 17:32:58 -0700

    store state initialized

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import {login, signup, logout} from './util/sessions_api_util';
 
 // import Root from './components/root';
-// import configureStore from './store/store';
+import configureStore from './store/store';
 //
 // import * as actions from './actions/post_actions';
 // import { selectAllPosts } from './reducers/selectors';
@@ -12,13 +12,9 @@ import {login, signup, logout} from './util/sessions_api_util';
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
 
-  window.login = login;
-  window.signup = signup;
-  window.logout = logout;
+  const store = configureStore();
 
-  // const store = configureStore();
-
-  // window.store = store;
+  window.store = store;
 
   ReactDOM.render(<h1>hiwerere</h1>, root);
 });
