c2ee04acf1823e0022930af85fee58bfe3d3f878
Vincent Chan <vincent.chan@biola.edu>
2017-05-21 21:23:58 -0700

    unstylized restaurant show page

--- frontend/components/restaurant/restaurant.jsx
+++ frontend/components/restaurant/restaurant.jsx
@@ -10,9 +10,19 @@ class Restaurant extends React.Component {
   }
 
   render() {
-    debugger;
+    const {restaurant} = this.props;
     return (
-      <div>{this.props.name}</div>
+      <div>
+        <img src={restaurant.img_url} />
+        {restaurant.name}
+        {restaurant.description}
+        {restaurant.location}
+        {restaurant.open_time}
+        {restaurant.close_time}
+        {restaurant.price}
+        {restaurant.rating}
+
+      </div>
     );
   }
 }
